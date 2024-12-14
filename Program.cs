7cebebebe7835ea952d0466bfedd43173426d889
Saraliev Khalid <saraliev_halid@mail.ru>
2024-12-14 03:04:16 +0300

    Добавил цикл для проходп по массиву

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿string[] inputArray = { "cat", "dog", "apple", "hi", "yes", "no", "banana", "pie" };
+string[] resultArray = new string[inputArray.Length];
+int resultCount = 0;
+
+foreach (var str in inputArray)
+{
+    if (str.Length <= 3)
+    {
+        resultArray[resultCount] = str;
+        resultCount++;
+    }
+}
+
 
